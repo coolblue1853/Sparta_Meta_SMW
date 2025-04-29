1236ab7eff2560824378b493387542d8cbcad56a
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2025-04-29 21:48:00 +0900

    feat : 카메라 경계 설정 완료
    - 카메라 경계 구역을 설정하는 것을 Pivot을 이용해 구현했습니다.

--- Assets/Scripts/Lobby/Camera/CameraManager.cs
+++ Assets/Scripts/Lobby/Camera/CameraManager.cs
@@ -11,9 +11,32 @@ public class CameraManager : MonoBehaviour
     [SerializeField]
     private Vector3 offset;
 
+    [SerializeField]
+    private GameObject cameraPivot;
+    private float  minX = 0;
+    private float  maxX = 0;
+    private float  minY = 0;
+    private float  maxY = 0;
+
+
     private void Awake()
     {
-        Screen.SetResolution(1280, 720, false); // 짝수 해상도 사용
+        // 카메라 경계 지정 함수, 추후 별도의 함수로 만들어야함
+        // 맵로드 이후 피벗 설정
+        if(cameraPivot != null)
+        {
+            GameObject leftUpPivot = cameraPivot.transform.GetChild(0).gameObject;
+            GameObject rightDownPivot = cameraPivot.transform.GetChild(1).gameObject;
+
+            float camHalfHeight = Camera.main.orthographicSize;
+            float camHalfWidth = camHalfHeight * ((float)Screen.width / Screen.height);
+
+
+            minX = leftUpPivot.transform.position.x + camHalfWidth;
+            maxX = rightDownPivot.transform.position.x - camHalfWidth;
+            minY = rightDownPivot.transform.position.y + camHalfHeight;
+            maxY = leftUpPivot.transform.position.y - camHalfHeight;
+        }
     }
 
     private void LateUpdate()
@@ -23,6 +46,9 @@ public class CameraManager : MonoBehaviour
         Vector3 destinationPosition = target.position + offset;
         Vector3 lerpPosition = Vector3.Lerp(transform.position, destinationPosition,
             lerpSpeed * Time.deltaTime);
+
+        lerpPosition.x = Mathf.Clamp(lerpPosition.x, minX, maxX);
+        lerpPosition.y = Mathf.Clamp(lerpPosition.y, minY, maxY);
 
         transform.position = lerpPosition;
     }
